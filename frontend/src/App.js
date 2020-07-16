e0dc02f3294d3643f2bd2a08c1e38720013e7cdb
Marc Tanas <mstanas@hotmail.com>
2020-07-16 19:01:36 +0300

    made privateroute for shop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, {useState, useContext, useEffect } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AppContext from './AppContext';
 import About from './About';
 import Blog from './Blog';
@@ -13,7 +13,19 @@ import Twekes from './Twekes';
 
 import './App.css';
 
-function App() {
+const PrivateRoute = ({ component: Component, ...otherProps }) => {
+
+  const [globalState, setGlobalState] = useContext(AppContext);
+
+  if(globalState.loggedIn) {
+    return(<Route component={Component} {...otherProps} />);
+  } else {
+    return(<Redirect to="/accounts/login" />);
+  }
+}
+
+
+const App = () => {
 
   const [globalState, setGlobalState] = useState({
     loggedIn: localStorage.getItem('jwt') ? true : false,
@@ -21,6 +33,15 @@ function App() {
     email: null
 
   });
+
+  useEffect(
+    ()=>{
+        // when (and if) globalState.loggedIn changes,
+        // run the below code
+        console.log("This the loggedIn state", globalState.loggedIn)
+    }, 
+    [globalState.loggedIn]
+  )
 
   return (
     <AppContext.Provider value={[globalState, setGlobalState]}>
@@ -33,7 +54,7 @@ function App() {
           <Route path="/accounts/login" exact={true} component={Login}/>
           <Route path="/accounts/passwordrecovery" exact={true} component={PasswordRecovery}/>
           <Route path="/legal/privacy" exact={true} component={Privacy}/>
-          <Route path="/shop" exact={true} component={Shop}/>
+          <PrivateRoute path="/shop" exact={true} component={Shop}/>
           <Route path="/legal/terms" exact={true} component={Terms}/>
         </Switch>
       </BrowserRouter>
